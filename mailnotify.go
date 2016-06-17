8acbba153970b8213084dafa31a9630ebe972411
Jens-Uwe Mager <jum@shadow.han.de>
2016-06-18 00:46:42 +0200

    go fmt and fix URL generation.

--- mailnotify.go
+++ mailnotify.go
@@ -1,38 +1,46 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "log"
-    "path/filepath"
+	"fmt"
+	"log"
+	"net/url"
+	"os"
+	"path/filepath"
 
-    "github.com/gregdel/pushover"
+	"github.com/gregdel/pushover"
 )
 
 func main() {
-    if len(os.Args) < 3 {
-        fmt.Printf("usage: %s api_key user_key\n", filepath.Base(os.Args[0]))
-        os.Exit(1)
-    }
-    app := pushover.New(os.Args[1])
+	if len(os.Args) < 3 {
+		fmt.Printf("usage: %s api_key user_key\n", filepath.Base(os.Args[0]))
+		os.Exit(1)
+	}
+	app := pushover.New(os.Args[1])
 
-    // Create a new recipient
-    recipient := pushover.NewRecipient(os.Args[2])
+	// Create a new recipient
+	recipient := pushover.NewRecipient(os.Args[2])
 
-    // Create the message to send
-    message := &pushover.Message{
-    Message:     os.Getenv("SUBJECT"),
-    Title:       os.Getenv("FROM"),
-    URL:         fmt.Sprintf("message:<%v>", os.Getenv("MSGID")),
-    URLTitle:    "Apple Mail",
-    }
+	// Create the message to send
+	message := &pushover.Message{
+		Message: os.Getenv("SUBJECT"),
+		Title:   os.Getenv("FROM"),
+	}
+	msgid := os.Getenv("MSGID")
+	if len(msgid) > 0 {
+		url := (&url.URL{
+			Scheme: "message",
+			Path:   msgid,
+		}).String()
+		message.URL = url
+		message.URLTitle = "Apple Mail"
+	}
 
-    // Send the message to the recipient
-    response, err := app.SendMessage(message, recipient)
-    if err != nil {
-        log.Panic(err)
-    }
-    if false {
-        log.Printf("pushover resp %v", response)
-    }
+	// Send the message to the recipient
+	response, err := app.SendMessage(message, recipient)
+	if err != nil {
+		log.Panic(err)
+	}
+	if false {
+		log.Printf("pushover resp %v", response)
+	}
 }
