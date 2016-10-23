fbdde2b2f8b5f185bcac8719f5a35b97f8150c96
Jens-Uwe Mager <jum@anubis.han.de>
2016-10-23 15:56:15 +0200

    Add RFC1342 decoding.

--- mailnotify.go
+++ mailnotify.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
+	"mime/quotedprintable"
 	"net/url"
 	"os"
 	"path/filepath"
+	"regexp"
+	"strings"
 
 	"github.com/gregdel/pushover"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/charmap"
+	"golang.org/x/text/encoding/unicode"
 )
 
 func main() {
@@ -20,10 +29,19 @@ func main() {
 	// Create a new recipient
 	recipient := pushover.NewRecipient(os.Args[2])
 
+	subject, err := decodeHeader(os.Getenv("SUBJECT"))
+	if err != nil {
+		log.Panic(err)
+	}
+	from, err := decodeHeader(os.Getenv("FROM"))
+	if err != nil {
+		log.Panic(err)
+	}
+
 	// Create the message to send
 	message := &pushover.Message{
-		Message: os.Getenv("SUBJECT"),
-		Title:   os.Getenv("FROM"),
+		Message: subject,
+		Title:   from,
 	}
 	msgid := os.Getenv("MSGID")
 	if len(msgid) > 0 {
@@ -43,4 +61,47 @@ func main() {
 	if false {
 		log.Printf("pushover resp %v", response)
 	}
+}
+
+func decoder(encoding string) (*encoding.Decoder, error) {
+	if strings.ToUpper(encoding) == "UTF-8" {
+		return unicode.UTF8.NewDecoder(), nil
+	} else if strings.ToUpper(encoding) == "ISO-8859-1" {
+		return charmap.ISO8859_1.NewDecoder(), nil
+	} else {
+		return nil, fmt.Errorf("Unknown encoding")
+	}
+}
+
+func decodeHeader(str string) (string, error) {
+	re := regexp.MustCompile(`\=\?(?P<charset>.*?)\?(?P<encoding>.*)\?(?P<body>.*?)\?(.*?)\=`)
+
+	matches := re.FindAllStringSubmatch(str, -1)
+	if len(matches) == 0 {
+		return str, nil
+	}
+
+	for _, match := range matches {
+		var r io.Reader = strings.NewReader(match[3])
+
+		if match[2] == "Q" {
+			r = quotedprintable.NewReader(r)
+		} else if match[2] == "B" {
+			r = base64.NewDecoder(base64.StdEncoding, r)
+		}
+
+		if d, err := decoder(match[1]); err == nil {
+			r = d.Reader(r)
+		}
+
+		if val, err := ioutil.ReadAll(r); err == nil {
+			str = strings.Replace(str, match[0], string(val), -1)
+		} else if err != nil {
+			fmt.Println(err.Error())
+			continue
+		}
+
+	}
+
+	return str, nil
 }
